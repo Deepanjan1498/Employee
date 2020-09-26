9627fc865c949ea332b7bda170ce95a1439efe37
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-09-26 21:49:51 +0530

    [Deepanjan]add . uc2 is solved.

--- EmpWageBuilderUC1.java
+++ EmpWageBuilderUC1.java
@@ -1,14 +1,19 @@
-
 public class EmpWageBuilderUC1
 {
-  public static void main(String[] args)
-  { //Constants
-    int IS_FULL_TIME = 1;
-    //Computation
-    double empCheck = Math.floor(Math.random() * 10) % 2;
-    if(empCheck == IS_FULL_TIME)
-         System.out.println("Employee is Present");
-      else
-         System.out.println("Employee is Absent");
-   }
+	public static void main(String args[])
+	{
+	 //constants
+	 int FULLTIME=1;
+	 int EMP_RATE_PER_HOUR=20;
+	 //variables
+	 int empHours=0;
+	 int empWage=0;
+ 	 int empCheck=(int)(Math.random()*10)%2;
+	 if(empCheck==FULLTIME)
+		empHours=8;
+	 else
+	        empHours=0;
+	 empWage=empHours*EMP_RATE_PER_HOUR;
+	 System.out.println("Employee Wage :"+empWage);
+	}
 }
